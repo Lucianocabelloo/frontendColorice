51cda766dee0d8d27873d26369d5196a24a63d00
lucianocabelloo <you@example.com>
2024-03-25 12:08:34 -0300

    Creado el form, y los queries de crear color y obtener color

--- src/Components/FormColors.jsx
+++ src/Components/FormColors.jsx
@@ -1,20 +1,59 @@
-import { useEffect, useState } from "react";
+import { useState } from "react";
 import Button from "react-bootstrap/Button";
 import Form from "react-bootstrap/Form";
 import FormImage from "./FormImage";
 import ListColors from "./ListColors";
+import { useForm } from "react-hook-form";
+import { crearColorAPI, listarColoresAPI } from "../helpers/queries";
 
 const FormColors = () => {
+  const [selectedColor, setSelectedColor] = useState("#563d7c");
+  const [colors, setColors] = useState([])
 
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+    setValue, 
+    reset
+  } = useForm();
+
+  const onSubmit = async (colorData) => {
+    const respuesta = await crearColorAPI(colorData)
+    if (respuesta.status === 201) {
+      console.info(`El color ha sido creado con exito`)
+      obtenerTareas()
+      reset()
+    } else {
+      console.error("Ha ocurrido un error")
+    }
+  };
+
+
+  const obtenerTareas = async () =>{
+    const respuesta = await listarColoresAPI()
+    if (respuesta.status === 200){
+      const data = await respuesta.json()
+      setColors(data)
+    } else {
+      console.error("Ha habido un error al obtener las tareas")
+    }
+  }
+
+  const handleColorChange = (event) => {
+    const { value } = event.target;
+    setSelectedColor(value); // Actualizar el estado con el nuevo color seleccionado
+    setValue("colorInput", value); // Establecer el valor del input de texto con el color seleccionado
+  };
 
   return (
     <>
       <section className="flex gap-8">
-        <div className="flex flex-col justify-center items-center bg-slate-100 p-3 rounded-md w-[100%] borde">
-          <h2 className=" text-2xl font-semibold">Administrar colores</h2>
+        <div className="flex flex-col justify-center items-center bg-slate-100 p-3 rounded-md w-[100%] border">
+          <h2 className="text-2xl font-semibold">Administrar colores</h2>
 
           <Form
-            onSubmit={handleAddColor}
+            onSubmit={handleSubmit(onSubmit)}
             className="flex gap-5 items-center w-[100%] justify-evenly"
           >
             <Form.Group className="mb-3 flex flex-col">
@@ -27,24 +66,27 @@ const FormColors = () => {
               <Form.Control
                 type="color"
                 id="exampleColorInput"
-                defaultValue="#563d7c"
+                value={selectedColor} 
                 title="Choose your color"
                 className="h-[100px] w-[100px]"
-                onChange={handleColorPicker}
+                onChange={handleColorChange}
+                
               />
             </Form.Group>
             <Form.Group className="flex gap-5">
               <Form.Control
-                onChange={handleChange}
-                value={change}
                 type="text"
                 className="w-[30vw] p-3 rounded-md"
                 placeholder="Ingrese el color en ingles"
+                {...register("colorInput", {
+                  required: false,
+                  minLength: 2,
+                  maxLength: 20,
+                })}
               />
-              {error && <span>{error}</span>}
             </Form.Group>
 
-            <Button className=" bg-orange-600 p-3 rounded-md" type="submit">
+            <Button className="bg-orange-600 p-3 rounded-md" type="submit">
               Seleccionar
             </Button>
           </Form>
--- src/helpers/queries.js
+++ src/helpers/queries.js
@@ -1,21 +1,61 @@
 const URL_API = import.meta.env.VITE_URL_API
 
-export const listarColoresAPI = ()=>{
+export const listarColoresAPI = async ()=>{
+    try {
+        const respuesta = await fetch(URL_API);
+        return respuesta;
+    } catch (error) {
+        console.log("Ha habido un error al listar colores", error);
+    }
+}
+
+export const listarColorAPI = async (id) =>{
+try {
+    const respuesta = await fetch((`${URI_Producto}/${id}`))
+    return respuesta
+} catch (error) {
+    console.error("Ha habido un error al listar color", error)
+}
+}
+
+export const crearColorAPI =  async (colorNuevo)=>{
+    try {
+        const respuesta = await fetch(URL_API,{
+            method: 'POST',
+            headers:{
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify(colorNuevo)
+        })
+        return respuesta
+    } catch (error) {
+        console.error("Ha habido un error al crear color", error)
+    }
 
 }
 
-export const listarColorAPI = () =>{
-
+export const editarColorAPI= async (colorNuevo, id) =>{
+    try {
+        const respuesta = await fetch(`${URL_API}/${id}`, {
+            method:'PUT',
+            headers: {
+                "Content-Type": "application/json",
+              },
+            body: JSON.stringify(colorNuevo)
+        })
+        return respuesta
+    } catch (error) {
+        console.error("Ha habido un error al editar el color", error)
+    }
 }
 
-export const crearColorAPI = ()=>{
-
-}
-
-export const editarColorAPI= () =>{
-
-}
-
-export const eliminarColorAPI = () =>{
-
+export const eliminarColorAPI = async (id) =>{
+    try {
+        const respuesta = await  fetch(`${URI_TAREA}/${id}`,{
+            method: "DELETE",
+        })
+        return respuesta
+    } catch (error) {
+        console.error("Ha habido un error al eliminar la tarea", error)
+    }
 }
